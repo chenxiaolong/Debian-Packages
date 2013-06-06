275fcae377acbae6d21eb3c78be2f7f25b2d00e8
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2013-06-06 12:28:25 -0400

    [freetype-infinality] Allow profile.d script to be parsed by sh

--- freetype-infinality/debian/freetype-infinality.profile.d
+++ freetype-infinality/debian/freetype-infinality.profile.d
@@ -1,8 +1,8 @@
-source /etc/default/freetype-infinality
+. /etc/default/freetype-infinality
 
 DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
-if [[ "${INFINALITY}" == true ]]; then
+if [ "x${INFINALITY}" = "xtrue" ]; then
   EXISTING=$(echo ${LD_PRELOAD} | grep "freetype.so" | grep -v "infinality" | wc -l)
   if [[ "${EXISTING}" == 0 ]]; then
     export LD_PRELOAD="/usr/lib/${DEB_HOST_MULTIARCH}/freetype-infinality/libfreetype.so.6.8.1:${LD_PRELOAD}"
