5faa542962f3bbdf16f4ab506abf01ef80458944
chenxiaolong <chenxiaolong@master>
2013-08-03 03:48:04 -0400

    [freetype-infinality] Second fix for bug #1

--- freetype-infinality/debian/freetype-infinality.profile.d
+++ freetype-infinality/debian/freetype-infinality.profile.d
@@ -4,7 +4,7 @@ DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
 if [ "x${INFINALITY}" = "xtrue" ]; then
   EXISTING=$(echo ${LD_PRELOAD} | grep "freetype.so" | grep -v "infinality" | wc -l)
-  if [[ "${EXISTING}" == 0 ]]; then
+  if [ "${EXISTING}" -eq 0 ]; then
     export LD_PRELOAD="/usr/lib/${DEB_HOST_MULTIARCH}/freetype-infinality/libfreetype.so.6.8.1:${LD_PRELOAD}"
   fi
 fi
